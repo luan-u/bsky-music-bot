9f60dc3ee449d9347f03e0102bbee937fecce012
Luan <luan_uchoa@outlook.com>
2025-03-17 11:50:10 -0400

    create snippet file

---
+++ text_store.py
@@ -0,0 +1,312 @@
+depot = [
+"""\
+Eu perguntava, tu e o holandês
+E te abraçava com o holandês
+🎶""",
+
+"""\
+Uma deusa, uma louca, uma betoneira
+Ela é demais
+🎶""",
+
+"""\
+Secretáriaaa
+Que trabalha o dia inteiro comigo
+Estou correndo grande perigo
+De parar no tribunal
+🎶""",
+
+"""\
+Gatinha, vem aqui pra conhecer
+O som que esse carro tem
+Eu sei que você vai gostar
+O som é de qualidade
+Você vai se amarrar
+
+Mas quem entra nesse carro é comida
+Por isso que o nome dele é Marmita
+🎶""",
+
+"""\
+Quero ovo de codorna pra comer
+O meu problema ele tem que resolver
+🎶""",
+
+"""\
+Foge, foge, Mulher Maravilha
+Foge, Foge, com o Superman
+🎶""",
+
+"""\
+Eu não sou cachorro não
+Pra viver tão humilhado
+Eu não sou cachorro não
+Para ser tão desprezado
+🎶""",
+
+"""\
+Nem que eu vá para a porrada, mas eu fico com você
+Bota pra moer, bota pra moer
+Hoje aqui nesse forró eu quero dançar com você
+Bota pra moer, bota pra moer
+🎶""",
+
+"""\
+Rapariga não, rapariga não
+Lave sua boca com água e sabão
+🎶""",
+
+"""\
+Não quero saber da sua vida, sua história
+Nem do seu passado
+
+Mulher de setenta
+Eu só quero ser
+O seu namorado
+🎶""",
+
+"""\
+Tudo o que Deus criou pensando em você
+Fez farinha láctea e deu pros dinossauros
+🎶""",
+
+"""\
+Em questão de Solimões 🍋
+Fundamental é saber 🧐
+Que o verde 🟢 não ❌ se mistura 🔄🔃
+Com o amarelo 🟡
+🎶""",
+
+"""\
+O meu pai foi peão 🤠
+Minha mãe foi peão 🤠
+Meus irmãos perderam-se na vida porque eram peão 🤠
+🎶""",
+
+"""\
+Meu amor
+Ah, se eu pudesse te traçar agora
+Poder parar o tempo nessa hora
+🎶""",
+
+"""\
+Eu sou nuvem passageira
+Que com o tempo se vai
+🎶""",
+
+"""\
+Diiiga que já não me queeeeer
+Neeeegue que me pertenceu
+🎶""",
+
+"""\
+Eu não presto, mas eu te amo
+Eu não presto, mas eu te amo
+🎶""",
+
+"""\
+Você é meu sócio, você é meu sócio
+Você investe e eu sou dono do negócio
+🎶""",
+
+"""\
+Today
+Is gonna be the day
+Like today is gonna be the day
+
+By day
+You should someday
+Realize that today is the day
+🎶""",
+
+"""
+Aumente o seu volume que o ciúme
+Não tem remédio
+Não tem remédio
+Não tem remédio não
+🎶""",
+
+"""\
+Gatinha 💃
+Cê gosta mais 🙎
+De Red Label 🥃
+Ou Ice🥤🧊? 
+🎶""",
+
+"""\
+Toma, gostosa, lapada na rachada
+Você pede e eu te dou lapada na rachada
+E aí, tá gostoso? (lapada na rachada)
+Toma, toma, toma
+🎶""",
+
+"""\
+Caaaaaaaaaaaaabaré
+
+É o melhor lugar pra tu esquecer tua ex-mulher
+É o melhor lugar pra tu esquecer tua ex-mulher
+🎶""",
+
+"""\
+Agarra o guaxinim
+Solta o guaxinim
+Agarra o guaxinim
+Solta o guaxinim
+Pega o guaxinim e agarra o guaxinim
+Pega o guaxinim e agarra
+🎶""",
+
+"""\
+Fuscão pretoooo
+Você é feito de açooooo
+🎶""",
+
+"""\
+Foi traído e quer se matar
+Ameaçou se jogar da sacada
+Não custa nada lembrar
+Isso é chifre, não é asa
+🎶""",
+
+"""\
+Diz pra eu ficar muda, faz cara de bermuda
+Tira essa bermuda que eu quero essa bermuda
+🎶
+""",
+
+"""\
+No hospital
+Na sala de cirurgia
+Pela vidraça eu via
+Você sofrendo a sorrir 🎶
+
+E o seu sorriso
+Aos poucos se desfazendo
+Eu vi você morrendo
+Sem poder me despedir 🎶""",
+
+"""\
+Renal, renal
+Eu tenho um problema
+Renal
+Renal
+🎶
+
+(Crowded House × nefrologista - Don't Dream It's Pedra No Rim)""",
+
+"""\
+Ela te chama de céu 🌌
+Ela leva teu anel 💍
+
+Ela te chama de pão 🍞
+Ela leva teu cordão 📿
+
+Ela te chama de amor 😍
+Vai sentir é muita dor 😩
+
+Ela te chama de meu bem 💏
+E leva tudo que tu tem 😶‍🌫️
+""",
+
+"""\
+Ele tá de olho é na butique delaaa
+É na butique delaaa
+🎶""",
+
+"""\
+A pílula já existia, mas nem se falava
+Pois dos muitos conselhos que sua mãe te dava
+Tinha um que dizia: só depois de casar
+🎶""",
+
+"""\
+Como Arceus
+Eu quero subir
+🎶""",
+
+"""\
+Depois de tudo que eu vi
+Não consigo esquecer
+Ela me disse adeus e se foi
+Nem seu nome eu sei dizer
+🎶""",
+
+"""
+É no cabeleireiro 
+no cabeleireiro 
+no cabeleireiro 
+no cabeleireiro 
+[...] 
+Final de semana
+no cabeleireiro
+Só gastando grana
+no cabeleireiro
+🎶""",
+
+"""\
+Aquela rua não é mais a mesma rua
+Ficou tão diferente desde que você mudou
+🎶""",
+
+"""
+Cachimblema
+Cachaça, chifre e problema
+🎶""",
+
+"""\
+só preciso de um dinheiro pra comprar um mé
+o leitin das criança e o modis da muié
+🎶""",
+
+"""\
+Não me mande embora, não sou sua amante
+Sou sua mulher e venho te buscar
+Você vai comigo por bem ou por mal
+Ou pego essas quengas e quebro no pau
+Eu rodo a baiana mas levo você
+🎶""",
+
+"""\
+Catingueira, catingueira🌵
+Diz o segredo que existe 🧐
+Que somente a catingueira🌵
+Enfeita a paisagem triste 😔
+🎶""",
+
+"""\
+Tell me why are we 
+So blind to see
+That the ones we hurt 
+Are you and me
+🎶""",
+
+"""Meu bem
+Você me dá
+Ferida na boca
+🎶""",
+
+"""\
+Tira a calça jeans, bota outra calça jeans
+Morena, você tem duas calças jeans
+🎶""",
+
+"""\
+Carcará 🦅
+Lá no sertão 🌵
+É um bicho que avoa 🪽 que nem avião ✈️
+É um pássaro 🐦 malvado 👿
+Tem o bico volteado ➰
+Que nem gavião 🦅""",
+
+"""\
+Carcará 🦅
+Pega 🪤 mata 🔪 e come 🍖
+Carcará 🦅
+Não vai 🚫 morrer ⚰️ de fome 😣
+Carcará 🦅
+Mais coragem 💪 do que homem 🙇
+Carcará 🦅
+Pega 🪤 mata 🔪 e come 🍖
+"""
+
+]
